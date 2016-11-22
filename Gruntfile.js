440cf2a067d4ba1c333add048fc69264f20622df
fabianonunes <fabiano@nunes.me>
2016-11-22 08:45:26 -0200

    remover phantom

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,25 +96,6 @@ module.exports = function (grunt) {
       }
     },
 
-    phantomcss: {
-      desktop: {
-        options: {
-          screenshots: 'tests/desktop/screenshots/',
-          results: 'tests/desktop/results/',
-          viewportSize: [1200, 1200]
-        },
-        src: ['tests/**/*desktop.js']
-      },
-      mobile: {
-        options: {
-          screenshots: 'tests/mobile/screenshots/',
-          results: 'tests/mobile/results/',
-          viewportSize: [480, 480]
-        },
-        src: ['tests/**/*desktop.js']
-      }
-    },
-
     postcss: {
       main: {
         options: {
@@ -161,7 +142,6 @@ module.exports = function (grunt) {
     'clean',
     'build',
     'connect',
-    'phantomcss',
     'clean'
   ])
 
